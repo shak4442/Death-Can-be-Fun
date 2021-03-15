c028d66fe81231dc4dbf7a65797ee2dda77c28fd
akhiljaison <74477703+akhiljaison@users.noreply.github.com>
2021-03-15 06:17:25 -0400

    Level advance script added

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+   public int index ;
+
+   void OnTriggerEnter2D(Collider2D other)
+    {
+        if (GameObject.FindWithTag("Coin") == null)
+        {
+            Debug.Log("exists");
+            if (other.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(index);
+            };
+        }
+       
+    }
+}
